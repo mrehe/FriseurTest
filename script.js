65aee64078f24219e0ec89c53c45535f3b340a3e
dirkdurchfall <chanvungo@hotmail.com>
2024-08-08 20:17:00 +0200

    V2

--- script.js
+++ script.js
@@ -38,44 +38,37 @@ document.addEventListener("DOMContentLoaded", () => {
     eventForm.addEventListener("submit", (e) => {
         e.preventDefault();
         const name = document.getElementById("eventName").value;
+        const service = document.getElementById("service").value;
         const date = document.getElementById("eventDate").value;
         const time = document.getElementById("eventTime").value;
-        addEvent(name, date, time);
+        addEvent(name, service, date, time);
         eventForm.reset();
         eventTime.innerHTML = '<option value="">Bitte Zeit auswählen</option>'; // Reset time slots
     });
 
-    function addEvent(name, date, time) {
-		
-		const formData = new FormData();
-            formData.append('name', 'Max');
-		
-		fetch('post_example.php', {
-                method: 'POST',
-                body: formData
-            })
+    function addEvent(name, service, date, time) {
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('service', service);
+        formData.append('date', date);
+        formData.append('time', time);
+
+        fetch('add_termin.php', {
+            method: 'POST',
+            body: formData
+        })
             .then(response => response.text())
             .then(data => {
-				console.log(data);
-            })
-            .catch(error => {
-                console.error('Fehler:', error);
-            });
-		
-        const request = new XMLHttpRequest();
-        request.open("POST", "add_termin.php", true);
-        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        request.onreadystatechange = function () {
-            if (this.readyState === XMLHttpRequest.DONE) {
-                if (this.status === 200) {
+                if (data === 'success') {
                     showAlert("Der Termin wurde für Sie gebucht, vielen Dank!", "success");
                     bootstrap.Modal.getInstance(document.getElementById('terminModal')).hide();
                 } else {
                     showAlert("Es gab ein Problem bei der Buchung des Termins. Bitte versuchen Sie es erneut.", "danger");
                 }
-            }
-        };
-        request.send(`name=${name}&date=${date}&time=${time}`);
+            })
+            .catch(error => {
+                showAlert("Es gab ein Problem bei der Buchung des Termins. Bitte versuchen Sie es erneut.", "danger");
+            });
     }
 
     function showAlert(message, type) {
